09724dab18e1eb260a544c163a8146e8acd787ea
Memia <memia@hotmail.com.au>
2018-08-16 10:28:17 +1000

    Week02
    We learnt how to make player movement using rigidbody.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rigid;
+    public float jumpHeight = 10f;
+    private bool isGrounded = true;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if W key is perssed
+        //moveforward
+        if (Input.GetKey(KeyCode.W))
+        {          
+            rigid.AddForce(Vector3.forward* speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigid.AddForce(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.AddForce(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigid.AddForce(Vector3.right * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            isGrounded = false;
+        }
+        
+    }
+
+    // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
