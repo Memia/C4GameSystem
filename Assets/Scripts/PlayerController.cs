349a9f1380e4bd53c51ccdcd622cdeeab703fc85
Memia <memia@hotmail.com.au>
2018-08-16 10:29:07 +1000

    Week03
    This lesson we refined the code, cleaned up movement(using rigidbody) and used raycast to do ground check, we also made it so the camera follows the player without rotating, also camera no longer needs to be attatched to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * 1. Ability
+ * 
+ * 
+ */
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float moveSpeed;
     public Rigidbody rigid;
     public float jumpHeight = 10f;
-    private bool isGrounded = true;
+    public float rayDistance = 1f;
+  
+
+    private void OnDrawGizmos()
+    {                           //the position where the script is attatched
+        Ray groundRay = new Ray(transform.position,Vector3.down);
+        //from groundRay.origin  to  groundRay.origin + groundRay.direction * rayDistance
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(groundRay.origin, groundRay.origin + groundRay.direction * rayDistance); 
+    }
+
     // Use this for initialization
     void Start()
     {
 
+    }
+    bool IsGrounded()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        //casts a line beneath the player
+        RaycastHit hit;
+        if (Physics.Raycast(groundRay, /* out gives information about ray cast hit*/out hit, rayDistance))
+        {
+            return true;
+        }
+        return false;
     }
 
     // Update is called once per frame
@@ -19,36 +44,58 @@ public class PlayerController : MonoBehaviour
     {
         //Check if W key is perssed
         //moveforward
+        #region If statement movement
+        /*
         if (Input.GetKey(KeyCode.W))
         {          
-            rigid.AddForce(Vector3.forward* speed);
+            rigid.AddForce(Vector3.forward* moveSpeed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rigid.AddForce(Vector3.back * speed);
+            rigid.AddForce(Vector3.back * moveSpeed);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rigid.AddForce(Vector3.left * speed);
+            rigid.AddForce(Vector3.left * moveSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rigid.AddForce(Vector3.right * speed);
+            rigid.AddForce(Vector3.right * moveSpeed);
         }
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
             isGrounded = false;
         }
-        
+        */
+        #endregion
+                            //GetAxisRaw snaps the numnber
+        float inputH = Input.GetAxis("Horizontal") *moveSpeed;
+        float inputV = Input.GetAxis("Vertical") *moveSpeed;
+                                            //this makes y 0 and creates a problem when you fall.
+        Vector3 moveDir = new Vector3(inputH, 0f, inputV);         
+        Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
+        if (Input.GetButton/*uses bool*/("Jump")&& IsGrounded(/*the brackets make sure they are ercognised as a function*/))
+        {
+            force.y = jumpHeight;
+        }
+
+        //velocity equals to movedire.x,y,z times movement speed, it creates a problem for y axis because gravity gets involved
+        rigid.velocity = force;
+        //if the user pressed a key (moveDir has values in it other than 0)
+        if (moveDir.magnitude > 0)
+        {       //rotate the player to that moveDir
+            transform.rotation = Quaternion.LookRotation(moveDir);
+        }
+
     }
 
-    // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider
+    /* OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.name == "Ground")
         {
             isGrounded = true;
         }
-    }
+    }*/
 }
---
+++ Assets/Scripts/WaypointEnemy.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class WaypointEnemy : MonoBehaviour
+
+{
+    public Transform waypointParent;
+    public NavMeshAgent agent;
+    public float stoppingDistance = 1f;
+    private Transform[] waypoints; //Creates a collection(array) of transforms
+    private int currentIndex = 1;
+    public Transform target;  //for seeking
+    public float seekRadius = 5f;
+    public enum State  //decleration
+        
+    { 
+        Patrol, Seek
+    }
+
+    public State currentState = State.Patrol; //definition
+    void Patrol()
+    {
+        Transform point = waypoints[currentIndex];
+        float distance = Vector3.Distance(transform.position, point.position);
+
+        if (distance < stoppingDistance)
+        {
+
+            // currentIndex = currentIndex +1
+            currentIndex++;
+        }//makes the index 1 if index reach/exceeds range, use this to create cycle
+        if (currentIndex >= waypoints.Length)
+        {
+            currentIndex = 1;
+        }
+        agent.SetDestination(point.position);
+        float distToTarget = Vector3.Distance(transform.position, target.position);
+        if (distToTarget < seekRadius)
+        {
+            currentState = State.Seek;
+        }
+
+    }
+    void Seek()
+    {
+        agent.SetDestination(target.position);
+        //transform.position = Vector3.MoveTowards(transform.position, point.position, moveSpeed);
+        float distToTarget = Vector3.Distance(transform.position, target.position);
+        if (distToTarget > seekRadius)
+        {
+            currentState = State.Patrol;
+        }
+    }
+
+    void Start()
+    {
+        //Getting children of waypointParent
+        waypoints = waypointParent.GetComponentsInChildren<Transform>();
+    }
+
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.Patrol:
+                //patrol statement
+                Patrol();
+                break;
+            case State.Seek:
+                Seek();
+                break;
+            default:
+                break;
+        }
+         //switch current state
+         //If we are in Patrol
+         // Call patrol()
+         //if  we are in seek
+         //Call Seek()
+
+    }
+}
